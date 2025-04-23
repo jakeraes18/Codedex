ea16bd7e3b5576f13e995edcf8d7f65ae0afb5cc
jasonramirez.ramirez.contractor <jasonadolfo.ramirez.contractor@bbva.com>
2025-04-23 12:48:08 -0600

    Your commit message here!

---
+++ 00-hello.py
@@ -0,0 +1,23 @@
+# Hola mundo
+"""
+Este es un comentario
+en varias
+lineas
+"""
+
+print('Hola Python')
+print("Hola Python")
+print('Hola Python')
+
+'''
+Este tambien
+es un comentario
+en varias lineas :D
+'''
+
+# Consultar el tipo de dato
+print(type('Hola Python')) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(10.34)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
---
+++ 01-variables.py
@@ -0,0 +1,15 @@
+# Variables
+
+my_variable = "My Variable string"
+print(my_variable)
+
+my_int_variable = 10
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+# Variables en una sola linea
+name, surname, alias, edad = 'Jason', 'Ramirez', 'Jake', 29
+print(f'Hola, {name} {surname}, se que tu alias es {alias} y que tu edad es: {edad} años')
+print('Hola', name, surname, 'tu alias es', alias, 'y tu edad son', edad, 'años')
